d59a73126f2ddbb3140fb446db810f03fb5d7df5
Thanh1999 <37361239+Thanh1999@users.noreply.github.com>
2018-04-02 22:09:46 +0700

    Update Ex1.java

--- Ex1.java
+++ Ex1.java
@@ -1,48 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//Exercise 1: Input an integer n, the program will show the first n primes
 package ex1;
 
 import java.util.Scanner;
+
 public class Ex1 {
 
-    
-public static void main(String[] args) {
-Scanner sc = new Scanner(System.in);
-int n = 0, number = 0;
-do{
-   try{
-       do{
-        System.out.print("Enter integer n: ");
-        n = sc.nextInt();
-        if(n <= 0)
-            System.out.println("Please input a positive number!");
-       }while(n <= 0);
-    int count = 0;
-    int num = 2;
-    while(count != n) { 
-    boolean prime = true;
-    for (int i = 2; i <= Math.sqrt(num); i++) { 
-        if (num % i == 0) {
-            prime = false; 
-            break;
-        }
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = 0, count = 0;
+        boolean flag = true;
+        do {
+            try {
+                do {  //Check input is positive number or not
+                    System.out.print("Enter integer n: ");
+                    n = Integer.parseInt(sc.nextLine());
+                    if (n <= 0) {
+                        System.out.println("Please input a positive number!");
+                    }
+                } while (n <= 0);
+                int num = 2;
+                while (count != n) {
+                    boolean prime = true;
+                    for (int i = 2; i <= Math.sqrt(num); i++) { //Check if num is prime or not
+                        if (num % i == 0) {
+                            prime = false;
+                            break;
+                        }
+                    }
+                    if (prime) {
+                        count++;
+                        System.out.printf("%d ", num); //Print prime num
+                    }
+                    num++;
+                }
+                flag = false;
+            } catch (Exception ex) {
+                System.out.println("Please input a number!");
+            }
+        } while (flag);
     }
-    if (prime) {
-        count++;
-        System.out.printf("%d ", num);
-    }
-    num++;
-    number++;
-}
-    }
-   catch(Exception ex){
-       System.out.println("Please input a number!");
-       sc.next();
-   }
-}while(number == 0);
-}
 }
