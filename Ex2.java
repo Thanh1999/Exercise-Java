e59232df2556a8c64760d69d6a551479000bb7d2
Thanh1999 <37361239+Thanh1999@users.noreply.github.com>
2018-04-03 10:17:20 +0700

    Update Ex2.java

--- Ex2.java
+++ Ex2.java
@@ -1,40 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//Exrcise 2: Show multiplication table of n 
 package ex2;
 
-/**
- *
- * @author Thanh
- */
 import java.util.Scanner;
+
 public class Ex2 {
+
     public static void main(String[] args) {
-    int n, i = 1, count = 0;
-    boolean test = true;
-    Scanner sc = new Scanner(System.in);
-    do{
-        try{
-            do{
-            System.out.print("Enter n: ");
-            n = sc.nextInt();
-            if(n < 0)
-            System.out.println("Error please input again!");
-            } while(n < 0);
-            System.out.printf("Multiplication table of %d:\n",n);
-            while(i <= 10){
-                String s = String.format("%d\tx\t%d\t=\t%d\n", n, i, n * i);
-                System.out.println(s);
-                i++;
+        int n = 0, i = 1;
+        boolean flag = true;
+        Scanner sc = new Scanner(System.in);
+        do {
+            try {
+                do {
+                    System.out.print("Enter n: ");
+                    n = Integer.parseInt(sc.nextLine());//Input integer n
+                    if (n < 0)//Check if n is positive number or not
+                    {
+                        System.out.println("Error please input again!");
+                    }
+                } while (n < 0);
+                System.out.printf("Multiplication table of %d:\n", n);
+                while (i <= 10) {//Print multiplication
+                    String equal = "=";
+                    String s = String.format("%-5dx%5d%5s%5d\n", n, i, equal, n * i);
+                    System.out.println(s);
+                    i++;
+                }
+                flag = false;//Loop until false
+            } catch (Exception ex) {//Check if n is number or not
+                System.out.println("Error please input again!");
             }
-            count++;
-        }
-        catch(Exception ex){
-            System.out.println("Error please input again!");
-            sc.next();
-        }
-} while(count == 0);
-       }
+        } while (flag);
+    }
 }
